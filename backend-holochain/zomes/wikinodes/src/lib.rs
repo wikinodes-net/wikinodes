e65a3214ace83902116049e276264563a71c34d6
Harlan T Wood <code@harlantwood.net>
2021-06-10 23:23:51 -1000

    Change to other sample zomes, simpler dev server, infra

--- backend-holochain/zomes/wikinodes/src/lib.rs
+++ backend-holochain/zomes/wikinodes/src/lib.rs
@@ -1,27 +1,12 @@
 use hdk::prelude::*;
 
 #[hdk_extern]
-pub fn who_am_i(_: ()) -> ExternResult<AgentPubKey> {
-    Ok(agent_info()?.agent_latest_pubkey)
+pub fn foo(_: ()) -> ExternResult<String> {
+    Ok(String::from("foo"))
 }
 
-// You need to send your input arguments via a serializable struct
-#[derive(Clone, Serialize, Deserialize, Debug, SerializedBytes)]
-#[serde(rename_all = "camelCase")]
-pub struct InputArgumentDTO {
-    pub content: String,
-}
-
-// It is recommended to send back the zome function result via a serializable struct
-#[derive(Clone, Serialize, Deserialize, Debug, SerializedBytes)]
-#[serde(rename_all = "camelCase")]
-pub struct HelloWorldResultDTO {
-    pub data: String,
-}
+// returns the current agent info
 #[hdk_extern]
-pub fn hello_world(input: InputArgumentDTO) -> ExternResult<HelloWorldResultDTO> {
-    let result = HelloWorldResultDTO {
-        data: input.content,
-    };
-    Ok(result)
+fn whoami(_: ()) -> ExternResult<AgentInfo> {
+    Ok(agent_info()?)
 }
