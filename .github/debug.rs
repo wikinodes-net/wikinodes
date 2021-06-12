dca98c4671df720136bd2950ff66e0deb00936d5
Harlan T Wood <code@harlantwood.net>
2021-06-11 15:14:02 -1000

    Debug rust date on macos CI

--- .github/debug.rs
+++ .github/debug.rs
@@ -6,10 +6,18 @@ fn main() {
   let now = std::time::Instant::now();
   println!("now: {:?}", now);
 
-  let some_one_day_ago = now.checked_sub(one_day);
-  println!("some_one_day_ago: {:?}", some_one_day_ago);
+  let one_day_ago_by_subtraction = now - one_day;
+  println!("one_day_ago_by_subtraction: {:?}", one_day_ago_by_subtraction);
 
-  let one_day_ago = some_one_day_ago.unwrap();
-  println!("one_day_ago: {:?}", one_day_ago);
+  let some_one_day_ago_by_checked_sub = now.checked_sub(one_day);
+  println!("some_one_day_ago_by_checked_sub: {:?}", some_one_day_ago_by_checked_sub);
 
+  let one_day_ago_by_checked_sub = some_one_day_ago_by_checked_sub.unwrap();
+  println!("one_day_ago_by_checked_sub: {:?}", one_day_ago_by_checked_sub);
+
+
+
+  // let max_nanoseconds = 0; // u64::MAX / 1_000_000_000;
+  // let duration = Duration::new(max_nanoseconds, 0);
+  // println!("{:?}", now + duration);
 }
