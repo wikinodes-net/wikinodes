acb4cf5697f447796ac4d2d0a272c871a7b25535
Harlan T Wood <code@harlantwood.net>
2021-06-12 18:36:43 -1000

    Refactor UI

--- ui/src/routes/index.svelte
+++ ui/src/routes/index.svelte
@@ -1,29 +1,69 @@
 <script lang="ts">
-  const projects = [
-    { name: 'Lorem ipsum dolor', funding: funding(), tags: tags() },
-    { name: 'Nam cursus nisl', funding: funding(), tags: tags() },
-    { name: 'Praesent feugiat neque', funding: funding(), tags: tags() },
-    { name: 'Vivamus venenatis risus', funding: funding(), tags: tags() },
-    { name: 'Vestibulum eleifend leo', funding: funding(), tags: tags() },
-    { name: 'Ut semper dolor', funding: funding(), tags: tags() },
-    { name: 'Duis molestie quam', funding: funding(), tags: tags() },
-    { name: 'Suspendisse non lectus', funding: funding(), tags: tags() },
-    { name: 'Quisque nec est', funding: funding(), tags: tags() },
-    { name: 'Cras eleifend eros', funding: funding(), tags: tags() },
-    { name: 'Duis luctus tellus', funding: funding(), tags: tags() },
-    { name: 'Ut pulvinar odio', funding: funding(), tags: tags() },
-    { name: 'Cras quis purus', funding: funding(), tags: tags() },
-    { name: 'In sed purus', funding: funding(), tags: tags() },
-    { name: 'In commodo nisl', funding: funding(), tags: tags() },
-    { name: 'Nunc egestas orci', funding: funding(), tags: tags() },
-    { name: 'Suspendisse eu dui', funding: funding(), tags: tags() },
-    { name: 'Morbi ac nulla', funding: funding(), tags: tags() },
-    { name: 'Etiam euismod nibh', funding: funding(), tags: tags() },
-    { name: 'Sed ut velit', funding: funding(), tags: tags() },
-    { name: 'Sed mollis mi', funding: funding(), tags: tags() },
-    { name: 'Nulla sed dolor', funding: funding(), tags: tags() },
-    { name: 'Nullam faucibus orci', funding: funding(), tags: tags() },
+  type FundingTarget = { name: string; funding: string; tags: string }
+  let projects: Array<FundingTarget> = []
+  $: {
+    projects = loadProjects()
+  }
+
+  const areas = [
+    'Acid Rain',
+    'Agricultural Pollution',
+    'Air Pollution',
+    'Climate Change',
+    'Deforestation & Logging',
+    'Disposal of Medical Waste',
+    'Effect on Marine Life',
+    'Genetic Modification',
+    'Global Warming',
+    'Household and Industrial Waste',
+    'Increased Carbon Footprint',
+    'Light and Noise Pollution',
+    'Littering and Landfills',
+    'Loss of Biodiversity',
+    'Loss of Endangered Species',
+    'Mining',
+    'Natural Disasters',
+    'Natural Resource Depletion',
+    'Nuclear Issues',
+    'Overpopulation',
+    'Ozone Layer Depletion',
+    'Public Health Issues',
+    'Soil regeneration',
+    'Urban Sprawl',
+    'Water Pollution',
   ]
+
+  const projectNames = [
+    'Lorem ipsum dolor',
+    'Nam cursus nisl',
+    'Praesent feugiat neque',
+    'Vivamus venenatis risus',
+    'Vestibulum eleifend leo',
+    'Ut semper dolor',
+    'Duis molestie quam',
+    'Suspendisse non lectus',
+    'Quisque nec est',
+    'Cras eleifend eros',
+    'Duis luctus tellus',
+    'Ut pulvinar odio',
+    'Cras quis purus',
+    'In sed purus',
+    'In commodo nisl',
+    'Nunc egestas orci',
+    'Suspendisse eu dui',
+    'Morbi ac nulla',
+    'Etiam euismod nibh',
+    'Sed ut velit',
+    'Sed mollis mi',
+    'Nulla sed dolor',
+    'Nullam faucibus orci',
+  ]
+
+  function loadProjects(): FundingTarget[] {
+    return projectNames.map((projectName) => {
+      return { name: projectName, funding: funding(), tags: tags() }
+    })
+  }
 
   function funding() {
     const dollars = Math.round((Math.random() + 0.5) * 10) * 75_000
@@ -41,34 +81,6 @@
   }
 
   function tags() {
-    const areas = [
-      'Acid Rain',
-      'Agricultural Pollution',
-      'Air Pollution',
-      'Climate Change',
-      'Deforestation & Logging',
-      'Disposal of Medical Waste',
-      'Effect on Marine Life',
-      'Genetic Modification',
-      'Global Warming',
-      'Household and Industrial Waste',
-      'Increased Carbon Footprint',
-      'Light and Noise Pollution',
-      'Littering and Landfills',
-      'Loss of Biodiversity',
-      'Loss of Endangered Species',
-      'Mining',
-      'Natural Disasters',
-      'Natural Resource Depletion',
-      'Nuclear Issues',
-      'Overpopulation',
-      'Ozone Layer Depletion',
-      'Public Health Issues',
-      'Soil regeneration',
-      'Urban Sprawl',
-      'Water Pollution',
-    ]
-
     const tags = []
     tags.push(areas[Math.floor(Math.random() * areas.length)])
     return tags.join(',')
