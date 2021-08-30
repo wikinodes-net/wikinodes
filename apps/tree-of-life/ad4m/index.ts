36e0be8c4f5b658e0a1111436fed5ffc616055de
Harlan T Wood <code@harlantwood.net>
2021-08-30 00:12:43 -1000

    Spike on Ad4mModel + associations

--- apps/tree-of-life/ad4m/index.ts
+++ apps/tree-of-life/ad4m/index.ts
@@ -4,6 +4,22 @@ import { ApolloClient, ApolloLink, InMemoryCache } from "@apollo/client";
 import { WebSocketLink } from "@apollo/client/link/ws";
 import ws from "ws";
 
+// class Foo {
+//   static hasManyAssociations = new Set();
+
+//   constructor(attrs: object) {
+//     console.log(this.constructor.name);
+//     console.log(this.constructor);
+//     console.log(Foo.hasManyAssociations)
+//     console.log(Bar.hasManyAssociations)
+//     console.log((this.constructor as any).hasManyAssociations)
+//   }
+// }
+
+// class Bar extends Foo {}
+
+// const bar = new Bar({});
+
 const uri = "http://localhost:4000/graphql";
 const apolloClient = new ApolloClient({
   link: new WebSocketLink({
@@ -20,7 +36,9 @@ const apolloClient = new ApolloClient({
 
 const ad4mClient = new Ad4mClient(apolloClient);
 
-(async function xxx() {
+main();
+
+async function main() {
   let isInitialized, isUnlocked, did, result;
 
   result = await ad4mClient.agent.status();
@@ -42,27 +60,112 @@ const ad4mClient = new Ad4mClient(apolloClient);
   }
 
   const languages = await ad4mClient.languages.all();
-  console.log({ languages });
+  // log({ languages });
 
-  const socialContextLanguage = languages.find(
-    (l) => l.name === "social-context"
-    // (l) => l.address === 'QmRfneighvZs6wc1fat2wNu2qVHddZDE7fcTRcVnsKa2ra'
-  );
-  if (!socialContextLanguage) throw new Error();
-  log({ socialContextLanguage });
+  const language = languages.find(
+    (l) => l.name === "note-ipfs"
+    // (l) => l.name === "social-context"
+  )?.address;
+  if (!language) throw new Error();
+  log({ language });
 
-  const perspective = await ad4mClient.perspective.add("My new space");
-  const createNeighbourhood =
-    await ad4mClient.neighbourhood.publishFromPerspective(
-      perspective.uuid,
-      socialContextLanguage.address,
-      new Perspective()
-    );
+  let treeOfLifePerspective;
+  treeOfLifePerspective = await ad4mClient.perspective.add("Tree of Life");
+  log({ treeOfLifePerspective });
 
-//     const addLink = await ad4mClient.perspective.addLink(perspective.uuid, {source: someExpressionAddress, target: someExpressionAddress2, predicate: 'p'})
-// const getLinks = await ad4mClient.perspective.queryLinks(perspective.uuid, {source: someExpressionAddress, predicate: ''p})
+  // languages, eg:
+  // TreeOfLife.v7.Funder
+  // relationships - use as predicates
+  // has many through - rich predicates?
 
-})();
+  class Funder extends Ad4mModel {
+    // static name: string
+  }
+  Funder.register();
+  Funder.hasMany("FundingEvent");
+
+  class FundingEvent extends Ad4mModel {}
+
+  // Generic sketch:
+  // type FundingEventFields =
+  // { fields:
+  //      name: string
+  //   associations:
+  //      hasMany:
+  // }
+  // class FundingEvent extends Ad4mModel<FundingEventFields> {}
+
+  const musk: any = new Funder({});
+  // maybe use generics?
+  // const musk: any = new Ad4mModel<Funder>({});
+  musk.FundingEvent.create({ name: "Musk Foundation 2022" });
+
+  // end of main
+}
+
+// user code above, lib code below
+
+abstract class Ad4mModel {
+  static hasManyAssociations = new Set();
+
+  constructor(attrs: object) {
+    console.log({'this.constructor.name': this.constructor.name});
+
+    // TODO create model as expression in ad4m
+
+    this.createHasManyAssociations();
+  }
+
+  private createHasManyAssociations() {
+    log((this.constructor as any).hasManyAssociations);
+    log((this.constructor as any).hasManyAssociations);
+    const otherModelNames: Array<string> = (this.constructor as any).hasManyAssociations;
+    for (const otherModelName in otherModelNames) {
+      if ((this as any)[otherModelName]) {
+        throw new Error(
+          `Already exists: <instance>${this.constructor.name}[${otherModelName}]`
+        );
+      }
+      // log({otherModelName});
+      (this as any)[otherModelName] = new Ad4mAssociationHasMany(
+        this.constructor.name,
+        otherModelName
+      );
+    }
+  }
+
+  // TODO move into constructor
+  static register() {
+    Ad4mModelRegistry.set(this.name, this);
+  }
+
+  static hasMany(otherModelName: string): void {
+    (this as any).hasManyAssociations.add(otherModelName);
+    console.log((this as any).hasManyAssociations);
+  }
+}
+
+class Ad4mModelRegistry {
+  static map: any = {};
+  static set(modelName: string, model: object): void {
+    this.map[modelName] = model;
+  }
+  static get(
+    modelName: string //: Ad4mModel
+  ) {
+    return this.map[modelName];
+  }
+}
+
+class Ad4mAssociationHasMany {
+  constructor(private thisModelName: string, private otherModelName: string) {}
+
+  create(attrs: object) {
+    const thisModel = Ad4mModelRegistry.get(this.thisModelName);
+    const otherModel = Ad4mModelRegistry.get(this.otherModelName);
+    // TODO create association in ad4m
+  }
+}
 
 function log(arg: object | string) {
   console.log(JSON.stringify(arg, null, 4));
