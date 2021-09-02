adfcabba13aa2e3a6fe9fc505c80a21712ee92cc
Harlan T Wood <code@harlantwood.net>
2021-09-02 11:50:50 -1000

    WIP models for TOL

--- apps/tree-of-life/ui/src/routes/index.svelte
+++ apps/tree-of-life/ui/src/routes/index.svelte
@@ -1,15 +1,17 @@
 <script context="module">
   export const ssr = false
-  // export const hydrate = false;
-  // export const router = false;
 </script>
 
 <script lang="ts">
 </script>
 
 <img src="/static/images/Tree-of-Life-FractalFund-Logo.png" alt="Tree of Life logo" />
-<a href="/tree-of-life/expert">Expert</a>
-<a href="/tree-of-life/budget">Budget</a>
+
+<h2>Active Funding Events</h2>
+<a href="/tree-of-life/funding-events/123">Ecocity Avalon</a>
+<!-- apps/tree-of-life/ui/src/routes/tree-of-life/funding-events/[address].json.ts -->
+<h2>Active Experts</h2>
+<a href="/tree-of-life/expert">Joshua Martin</a>
 
 <style>
 </style>
---
+++ apps/tree-of-life/ui/src/routes/tree-of-life/funding-events/[address].svelte
@@ -0,0 +1,1 @@
+<h1>Funding Event</h1>
